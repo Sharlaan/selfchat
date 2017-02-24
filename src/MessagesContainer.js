f3ee56c7fbf1894137a2adc639b4a59d20721a53
Raphaël Morineau <raphael.morineau@gmail.com>
2017-02-24 19:00:29 +0100

    Fixed scrolling issue when list items overflowing.

--- src/MessagesContainer.js
+++ src/MessagesContainer.js
@@ -1,17 +1,29 @@
-import React, { PropTypes } from 'react'
+import React, { Component, PropTypes } from 'react'
+import { findDOMNode } from 'react-dom'
 import List from 'material-ui/List/List'
 import Subheader from 'material-ui/Subheader/Subheader'
 
 import Message from './Message'
 
-function MessagesContainer ({ messages }) {
-  return (
-    <List style={{ flex: 1 }}>
-      <Subheader>Aujourd'hui</Subheader>
-      {messages.map(({ from, date, content }, index) =>
-        <Message from={from} date={date} key={index} content={content} />)}
-    </List>
-  )
+class MessagesContainer extends Component {
+  componentDidUpdate () {
+    this.scrollToBottom()
+  }
+
+  scrollToBottom = () => {
+    const list = findDOMNode(this.list)
+    list.scrollTop = list.scrollHeight
+  }
+
+  render () {
+    return (
+      <List style={{ flex: 1, overflowY: 'auto' }} ref={ref => (this.list = ref)}>
+        <Subheader>Aujourd'hui</Subheader>
+        {this.props.messages.map(({ from, date, content }, index) =>
+          <Message from={from} date={date} key={index} content={content}/>)}
+      </List>
+    )
+  }
 }
 
 MessagesContainer.propTypes = {
